223f849e2e6264e89e51aff34dac7186ca56e248
Muhammad Ra'if Alkautsar <96478279+lotsadelight@users.noreply.github.com>
2024-10-17 01:58:08 +0700

    feat: added multiple linear regression

--- src/regression/MultipleLinearRegression.java
+++ src/regression/MultipleLinearRegression.java
@@ -1,49 +1,64 @@
 package regression;
-
 import matrix.Matrix;
+import matrix.MatrixSolver;
+
+// BRAINSTORM ALGORITMA
 
 // pengisian baris paling atas
+// inisialisasi matriks ukuran m+1 x m+2
+
 // matrixAugmented[0][0] = n
-// for (int j = 0; j < m; j++)
+// for (int k_kanan = 0; k_kanan < m+1; k++)
 //      element_sum = 0;
-//      for (int k = 0; k < n; k++)
-//          element_sum += tuples[k][j]
-//      matrixAugmented[0][j+1] = element_sum
-// matrixAugmented[0][m+1] = n * tuples[0][m]
+//      for (int i = 0; i < n; i++)
+//          element_sum += tuples[i][k_kanan]
+//      matrixAugmented[0][k_kanan+1] = element_sum
 
 // pengisian baris-baris seterusnya
-// for (int i = 1; i < m; i++)
-//      for (int j = 0; j < n; j++)
-//          matrixAugmented[i][0] += tuples[j][i-1]
-//      for (int
+// for (int k_bawah = 0; k_bawah < m; k_bawah++)
+//      for (int i = 0; i < n; i++)
+//          matrixAugmented[k_bawah+1][0] += tuples[i][k_bawah]
+//      for (int k_kanan = 0; k_kanan < m + 1; k_kanan++)
+//          for (int i = 0; i < n; i++)
+//              matrixAugmented[k_bawah+1][k_kanan+1] += tuples[i][k_bawah] * tuples[i][k_kanan]
 
-// x1 x2 x3 x4 y
-// 6  7  8  9  5
-// 4  2  3  9  3
+// solve using gauss elimination
+// return array
 
+// IMPLEMENTASI
 public class MultipleLinearRegression {
     public static double[] calculateEstimationEquation(double[][] tuples, int n, int m, double x) {
+        // Buat matriks augmented berukuran m+1 x m+2
+        Matrix matrixAugmented = new Matrix(m+1,  m+2);
 
-        Matrix matrixAugmented = new Matrix(n, n+1);
+        // Pengisian baris pertama
+        matrixAugmented.setElement(0, 0, n);
+        for (int k_kanan = 0; k_kanan < m+1; k_kanan++) {
+            double element_sum = 0;
+            for (int i = 0; i < n; i++) {
+                element_sum += tuples[i][k_kanan];
+            }
+            matrixAugmented.setElement(0, k_kanan+1, element_sum); // Set y value
+        }
 
-        matrixAugmented.setElement(0,0,n);
-        for (int j = 0; j < m; j++) {
-            int element_sum = 0;
-            for (int k=0; k < n; k++) {
-                element_sum += tuples[j-1][k];
+        // Pengisian baris-baris seterusnya
+        for (int k_bawah = 0; k_bawah < m; k_bawah++) {
+            double element_sum = 0;
+            for (int i = 0; i < n; i++) {
+                element_sum += tuples[i][k_bawah];
+            }
+            matrixAugmented.setElement(k_bawah + 1, 0, element_sum);
+            for (int k_kanan = 0; k_kanan < m+1; k_kanan++) {
+                element_sum = 0;
+                for (int i = 0; i < n; i++) {
+                    element_sum += tuples[i][k_bawah] * tuples[i][k_kanan];
+                }
+                matrixAugmented.setElement(k_bawah+1,k_kanan+1, element_sum);
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrixAugmented.setElement(i, j, tuples[i][0]); // dipangkatkan
-            }
-            matrixAugmented.setElement(i, n, tuples[i][1]);
-        }
-
-        // Solve dengan eliminasi Gauss-Jordan atau Kaidah Cramer.
-
-        return new double[n];
+        MatrixSolver matrixToSolve = new MatrixSolver(matrixAugmented);
+        return matrixToSolve.gaussElimination();
     }
 
     public static double calculateY(double[] coefficients) {
